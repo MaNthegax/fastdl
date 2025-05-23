d372dd7da42926d3d00936fad6bb18a86a2026d0
Celio Lozatto <lozatto@outlook.com>
2025-05-23 17:59:30 -0300

    added floodgate

--- compactador.bat
+++ compactador.bat
@@ -1,30 +1,46 @@
 @echo off
-REM Ensures variables set within the batch file are local to
-REM this script only and will be removed when finished.
-setlocal
+setlocal enabledelayedexpansion
 
-REM Altere o caminho abaixo para a pasta onde o script deve começar
+REM Caminho do 7-Zip
+set "compressor=C:\Program Files\7-Zip\7z.exe"
+
+REM Extensão a ser gerada (.gz por padrão)
+set "extension=gz"
+
+REM Verifica se o 7-Zip existe
+if not exist "%compressor%" (
+    echo [ERRO] 7-Zip nao encontrado em: %compressor%
+    pause
+    exit /b
+)
+
+REM Define o diretório inicial como a subpasta \cs16 do .bat
 cd /D "%~dp0\cs16"
+
+echo Iniciando compactacao recursiva em: %CD%
+echo Usando compressor: %compressor%
+echo.
 
 REM Compacta todos os arquivos recursivamente, exceto .gz e .cmd
 for /r %%X in (*) do (
-    REM Pula arquivos com extensão .gz ou .cmd
-    if /I not "%%~xX"==".gz" if /I not "%%~xX"==".cmd" (
-        REM Verifica se é um arquivo (não diretório)
-        if exist "%%X" (
-            set "gzPath=%%X.gz"
-            REM Verifica se o arquivo .gz ainda não existe
-            if not exist "!gzPath!" (
-                echo Compactando: %%X
-                "C:\Program Files\7-Zip\7z.exe" a -tgzip "!gzPath!" "%%X" >nul
-            ) else (
-                echo Já compactado, ignorando: %%X
-            )
+    REM Extensao atual do arquivo
+    set "ext=%%~xX"
+    
+    REM Ignora arquivos .gz e .cmd
+    if /I not "!ext!"==".%extension%" if /I not "!ext!"==".cmd" (
+        REM Define o arquivo .gz de destino
+        set "gzFile=%%X.%extension%"
+
+        REM Só compacta se o arquivo .gz ainda não existir
+        if not exist "!gzFile!" (
+            echo Compactando: %%X
+            "%compressor%" a -tgzip "!gzFile!" "%%X" >nul
+        ) else (
+            echo Ja existe: !gzFile!
         )
     )
 )
 
 echo.
-echo Operação finalizada.
+echo Compactacao finalizada.
 pause
-
