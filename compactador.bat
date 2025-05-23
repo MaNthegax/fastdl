f6c52dd1ae69dd41ea573f18728950b3d69be395
Celio Lozatto <lozatto@outlook.com>
2025-05-23 17:37:11 -0300

    added floodgate

--- compactador.bat
+++ compactador.bat
@@ -6,17 +6,19 @@ setlocal
 REM Altere o caminho abaixo para a pasta onde o script deve começar
 cd /D "%~dp0\cs16"
 
-REM Compacta todos os arquivos recursivamente, ignorando .gz, .cmd e pastas
+REM Compacta todos os arquivos recursivamente, exceto .gz e .cmd
 for /r %%X in (*) do (
-    REM Verifica se NÃO é um arquivo .gz ou .cmd
+    REM Pula arquivos com extensão .gz ou .cmd
     if /I not "%%~xX"==".gz" if /I not "%%~xX"==".cmd" (
-        REM Verifica se é um arquivo (não uma pasta)
+        REM Verifica se é um arquivo (não diretório)
         if exist "%%X" (
-            echo Compactando: %%X
-            "C:\Program Files\7-Zip\7z.exe" a -tgzip "%%X.gz" "%%X" >nul
-            if exist "%%X.gz" (
-                echo Excluindo original: %%X
-                del "%%X"
+            set "gzPath=%%X.gz"
+            REM Verifica se o arquivo .gz ainda não existe
+            if not exist "!gzPath!" (
+                echo Compactando: %%X
+                "C:\Program Files\7-Zip\7z.exe" a -tgzip "!gzPath!" "%%X" >nul
+            ) else (
+                echo Já compactado, ignorando: %%X
             )
         )
     )
---
+++ cs16/maps/zm_cave_new.res
@@ -0,0 +1,25 @@
+// zm_cave_new.res - created with RESGen v2.0 BETA 3.
+// RESGen is made by Jeroen "ShadowLord" Bogers.
+// For more info go to http://resgen.hltools.com
+// or E-mail me at resgen@hltools.com.
+
+// .res entries (43):
+gfx/env/trainyardup.tga
+gfx/env/trainyarddn.tga
+gfx/env/trainyardlf.tga
+gfx/env/trainyardrt.tga
+gfx/env/trainyardft.tga
+gfx/env/trainyardbk.tga
+sound/weapons/m249-1.wav
+models/zm_cave_new/tree_pine.mdl
+models/zm_cave_new/bush05.mdl
+models/zm_cave_new/hessi_gras01.mdl
+sound/zm_cave_new/windmill_wind.wav
+sound/zm_cave_new/cave_indoor.wav
+sound/zm_cave_new/trainpass.wav
+sound/zm_cave_new/town_zombie.wav
+sound/zm_cave_new/strange_talk.wav
+sound/zm_cave_new/terrain_rumble.wav
+sound/zm_cave_new/scared_breathing.wav
+overviews/zm_cave_new.bmp
+overviews/zm_cave_new.txt
